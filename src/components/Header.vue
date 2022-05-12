b85958e34f6944086620aca5644f315404a949d6
AntunBelic <antunbelic93@gmail.com>
2022-05-12 15:39:01 +0200

    halfsetup

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h1>Tastk Tracker</h1>
+        <Button text="Add Task"/>
+    </header>
+</template>
+<script>
+import Button from "./Button.vue"
+export default {
+    name:"Header",
+    props: {
+        title:String,
+        showAddTask:Boolean,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
